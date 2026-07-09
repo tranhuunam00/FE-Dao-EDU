f4881e8fc105787605bcaa14f8c4261f9bc8a496
Namth <tranhuunam23022000@gmail.com>
2026-07-09 16:37:32 +0700

    release v.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as fs from 'fs'
 import * as path from 'path'
@@ -22,7 +22,7 @@ function loadDotEnv(root: string): Record<string, string> {
 }
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   const envVars = loadDotEnv(__dirname)
 
   return {
